14f7e4d50f68d5fb5cdf744281f83d156e9ab04c
NatiEfraim <nati.efraim@gmail.com>
2025-01-14 11:11:37 +0200

    Demo data for upload file

--- routes/uploads.js
+++ routes/uploads.js
@@ -4,12 +4,12 @@ const {ProductModel} = require("../models/productModel");
 const { auth } = require("../middlewares/auth");
 const router = express.Router();
 
-// בפרוייקט אמיתי הערכים של המשתנים צריכים להיות ב
-// .ENV
+
+// Configure Cloudinary using secrets
 cloudinary.config({
-  cloud_name: "dccoiwwxy",
-  api_key: "66497גדש6928",
-  api_secret: "dZhגדש8k9OB60HO9zVprjpI"
+  cloud_name: config.CLOUDINARY.CLOUD_NAME,
+  api_key: config.CLOUDINARY.API_KEY,
+  api_secret: config.CLOUDINARY.API_SECRET,
 });
 
 router.get("/", async(req,res) => {
@@ -20,13 +20,8 @@ router.post("/product/:id",auth, async(req,res) => {
   try{
     const myFile = req.body.myFile;
     if(myFile){
-      // מעלה את התמונה לקלואדינרי 
       const data = await cloudinary.uploader.upload(myFile ,{ unique_filename:true})
-      // console.log(myFile);
-      // יחזיר פרטים על התמונה שנמצאת בשרת כולל הכתובת שלה
-      // ב secure_url
       const id = req.params.id;
-      // מעדכנים במסד את היו אר אל של התמונה שעלתה לקלאוד
       const dataProduct = await ProductModel.updateOne({_id:id,user_id:req.tokenData._id},{img_url:data.secure_url})
       res.json(dataProduct)
       
@@ -42,11 +37,7 @@ router.post("/cloud_server", async(req,res) => {
   try{
     const myFile = req.body.myFile;
     if(myFile){
-      // מעלה את התמונה לקלואדינרי 
       const data = await cloudinary.uploader.upload(myFile ,{ unique_filename:true})
-      // console.log(myFile);
-      // יחזיר פרטים על התמונה שנמצאת בשרת כולל הכתובת שלה
-      // ב secure_url
       res.json(data)
 
     }
@@ -61,11 +52,8 @@ router.post("/cloud1", async(req,res) => {
   try{
     const myFile = req.files.myFile;
     if(myFile){
-      // מעלה את התמונה לקלואדינרי 
       const data = await cloudinary.uploader.upload(myFile.tempFilePath ,{ unique_filename:true})
-      // console.log(myFile);
-      // יחזיר פרטים על התמונה שנמצאת בשרת כולל הכתובת שלה
-      // ב secure_url
+
       res.json(data)
 
     }
